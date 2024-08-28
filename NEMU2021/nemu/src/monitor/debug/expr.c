cb2ed8efd5fd893e738ee5fb919606f5a1b41842
3023244355-Yu xinghang <3023244355@tju.edu.cn>
2024-08-28 21:37:11 +0800

    NEMU2022

---
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -0,0 +1,157 @@
+#include "nemu.h"
+
+/* We use the POSIX regex functions to process regular expressions.
+ * Type 'man regex' for more information about POSIX regex functions.
+ */
+#include <sys/types.h>
+#include <regex.h>
+
+enum {
+	NOTYPE = 256,
+	NUM = 1,
+	ADD = 2,
+	SUB = 3,
+	MUL = 4,
+	DIV = 5,
+	LEFT = 6 ,
+	RIGHT = 7,
+	EQ = 8
+
+	/* TODO: Add more token types */
+
+};
+
+static struct rule {
+	char *regex;
+	int token_type;
+} rules[] = {
+
+	/* TODO: Add more rules.
+	 * Pay attention to the precedence level of different rules.
+	 */
+
+	{"[0-9]+", NUM},            // 数字
+	{" +", NOTYPE},           // 空白字符
+    {"\\+", ADD},             // 加号
+    {"-", SUB},               // 减号
+    {"\\*", MUL},             // 乘号
+    {"/", DIV},               // 除号
+    {"\\(", LEFT},            // 左括号
+    {"\\)", RIGHT},           // 右括号
+    {"==", EQ},               // 等于
+};
+
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+
+static regex_t re[NR_REGEX];
+
+/* Rules are used for many times.
+ * Therefore we compile them only once before any usage.
+ */
+void init_regex() {
+	int i;
+	char error_msg[128];
+	int ret;
+
+	for(i = 0; i < NR_REGEX; i ++) {
+		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+		if(ret != 0) {
+			regerror(ret, &re[i], error_msg, 128);
+			Assert(ret == 0, "regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+		}
+	}
+}
+
+typedef struct token {
+	int type;
+	char str[32];
+} Token;
+
+Token tokens[32];
+int nr_token;
+
+static bool make_token(char *e) {
+	int position = 0;
+	int i;
+	regmatch_t pmatch;//储存匹配结果
+	
+	nr_token = 0;//已有的token数量
+
+	while(e[position] != '\0') {
+		/* Try all rules one by one. */
+		for(i = 0; i < NR_REGEX; i ++) {//遍历所有正则表达式规则
+			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+				char *substr_start = e + position;//匹配成功子串的起始地址
+				int substr_len = pmatch.rm_eo;//匹配成功字符串长度
+
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				//匹配成功的日志
+				position += substr_len;
+
+				/* TODO: Now a new token is recognized with rules[i]. Add codes
+				 * to record the token in the array `tokens'. For certain types
+				 * of tokens, some extra actions should be performed.
+				 */
+				if(nr_token<32){
+					strncpy(tokens[nr_token].str,substr_start,substr_len);//复制一段
+					tokens[nr_token].str[substr_len-1]='\0';//给字符串结尾
+					tokens[nr_token].type=rules[i].token_type;
+					nr_token++;
+				}
+				switch(rules[i].token_type) {
+					case NOTYPE:break;
+					case NUM:break;
+					case ADD:break;
+					case SUB:break;
+					case MUL:break;
+					case DIV:break;
+					case LEFT:break;
+					case RIGHT:break;
+					case EQ:break;
+					default: panic("please implement me");
+				}
+
+				break;
+			}
+		}
+
+		if(i == NR_REGEX) {
+			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+			return false;
+		}
+	}
+
+	return true; 
+}
+
+uint32_t expr(char *e, bool *success) {
+	if(!make_token(e)) {
+		*success = false;
+		return 0;
+	}
+
+	/* TODO: Insert codes to evaluate the expression. */
+	eval(p, q) {
+		if (p > q) {
+			/* Bad expression */
+		}
+		else if (p == q) {
+			/* Single token.
+			* For now this token should be a number.
+			* Return the value of the number.
+			*/
+		}
+		else if (check_parentheses(p, q) == true) {
+			/* The expression is surrounded by a matched pair of parentheses.
+			* If that is the case, just throw away the parentheses.
+			*/
+			return eval(p + 1, q - 1);
+		}
+		else {
+			/* We should do more things here. */
+		}
+	}
+	panic("please implement me");
+	return 0;
+}
+
---
+++ NEMU2021/nemu/src/monitor/debug/ui.c
@@ -0,0 +1,153 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint32_t);
+static int cmd_si(char * s){
+	if(s==NULL){
+		cpu_exec(1);
+		return 0;
+	}
+	int p;
+	sscanf(s,"%d",&p);
+	cpu_exec(p);
+	return 0;
+}
+static int cmd_info(char* s){
+	if(strcmp(s,"r")==0){
+		int i;
+		for(i=R_EAX;i<=R_EDI;i++){
+			printf("%s\t0x%.8x\t%d\n",regsl[i],reg_l(i),reg_l(i));
+		}
+		printf("$eip\t0x%08x\t%d\n", cpu.eip,cpu.eip);
+	}else if(strcmp(s,"w")){
+
+	}
+	return 0;
+}
+static int cmd_x(char* s){
+	int n,x1;
+	sscanf(s,"%d %x",&n,&x1);
+	int p;
+	for(p=0;p<n;p++){
+		if(p%4==0) printf("0x%.8x: ",x1);
+		printf("0x%.8x ",swaddr_read(x1,4));
+		x1+=4;
+		if(p%4==3) printf("\n");
+		else if(p==n-1) printf("\n");
+	}
+	return 0;
+}
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+char* rl_gets() {
+	static char *line_read = NULL;
+
+	if (line_read) {
+		free(line_read);
+		line_read = NULL;
+	}
+
+	line_read = readline("(nemu) ");
+
+	if (line_read && *line_read) {
+		add_history(line_read);
+	}
+
+	return line_read;
+}
+
+static int cmd_c(char *args) {
+	cpu_exec(-1);
+	return 0;
+}
+
+static int cmd_q(char *args) {
+	return -1;
+}
+
+static int cmd_help(char *args);
+
+static int cmd_p(char *args){
+	bool success=true;
+	bool* p=&success;
+	expr(args,p);
+	return 0;
+}
+static struct {
+	char *name;
+	char *description;
+	int (*handler) (char *);
+} cmd_table [] = {
+	{ "help", "Display informations about all supported commands", cmd_help},
+	{ "c", "Continue the execution of the program",cmd_c},
+	{ "q", "Exit NEMU",cmd_q},
+	/* TODO: Add more commands */
+	{"si", "Next step",cmd_si},
+	{"info","print all reg status",cmd_info},
+	{"x","show next 'n' adress",cmd_x},
+	{"p","Analyze regular expression",cmd_p}
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_help(char *args) {
+	/* extract the first argument */
+	char *arg = strtok(NULL, " ");
+	int i;
+
+	if(arg == NULL) {
+		/* no argument given */
+		for(i = 0; i < NR_CMD; i ++) {
+			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		}
+	}
+	else {
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(arg, cmd_table[i].name) == 0) {
+				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+				return 0;
+			}
+		}
+		printf("Unknown command '%s'\n", arg);
+	}
+	return 0;
+}
+
+void ui_mainloop() {
+	while(1) {
+		char *str = rl_gets();
+		char *str_end = str + strlen(str);
+
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if(cmd == NULL) { continue; }
+
+		/* treat the remaining string as the arguments,
+		 * which may need further parsing
+		 */
+		char *args = cmd + strlen(cmd) + 1;
+		if(args >= str_end) {
+			args = NULL;
+		}
+
+#ifdef HAS_DEVICE
+		extern void sdl_clear_event_queue(void);
+		sdl_clear_event_queue();
+#endif
+
+		int i;
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(cmd, cmd_table[i].name) == 0) {
+				if(cmd_table[i].handler(args) < 0) { return; }
+				break;
+			}
+		}
+
+		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+	}
+}
