5a08b9655ac1a076189424aa1ca41498fd89f098
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 16:39:39 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:39:39 up  3:22,  1 user,  load average: 0.06, 0.06, 0.02 9e62b7633c7ff0934871602caccec2c62890fc

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -207,7 +207,7 @@ uint32_t find_domanit(uint32_t p,uint32_t q){//找主运算符
                     }
 			}
 		}
-		// printf("token%d: %s,anspos:%d,fu:%d\n",tokens[i].type,tokens[i].str,anspos,tokens[i].fu);
+		printf("token%d: %s,anspos:%d,fu:%d\n",tokens[i].type,tokens[i].str,anspos,tokens[i].fu);
 	}
 	return anspos;
 }
