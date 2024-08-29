a193fda22b34091801d75aba08d0e80cf27fc7bb
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 12:26:58 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:26:58 up 54 min,  1 user,  load average: 0.02, 0.04, 0.00 85bf6c375bad3716355ef44474c67432e382a6e1

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -123,7 +123,7 @@ static bool make_token(char *e) {
 			return false;
 		}
 	}
-
+	printf("number of token:%d\n",nr_token);
 	return true; 
 }
 // uint32_t eval(p, q) {
@@ -151,7 +151,6 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
-
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
 	return 0;
