0c64ba4855dc62532208b0128d630748b726ff93
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 16:42:18 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:42:18 up  3:24,  1 user,  load average: 0.08, 0.06, 0.01 9ba7dfa828f8ed16793fcb1bbb2862b522d41e7

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -22,7 +22,6 @@ enum {
 	OR = 11,
 	NOT = 12,
 	XING = 13,//解引用
-
 };
 
 static struct rule {
@@ -120,7 +119,7 @@ static bool make_token(char *e) {
 					assert(0);
 				}
 				// fu=0;
-				if(strcmp(tokens[nr_token-1].str,"-")==0&&(nr_token==1||tokens[nr_token-2].type!=NUM)){//如果当前是负号，且上一个为空或不是数字
+				if(strcmp(tokens[nr_token-1].str,"-")==0&&(nr_token==1||(tokens[nr_token-2].type!=NUM&&tokens[nr_token-2].type!=RIGHT))){//如果当前是负号，且上一个为空或不是数字
 					fu=1;
 					nr_token--;
 				}
