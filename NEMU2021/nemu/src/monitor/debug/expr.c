a5cb549a8f81fd5dd3a4f5cd5fec2ade5f90614e
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 14:27:45 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:27:45 up  1:10,  1 user,  load average: 0.04, 0.07, 0.04 f0d6c5ae9fde77cee80dbe221b8b0bfbb39178b

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -141,6 +141,7 @@ uint32_t find_domanit(uint32_t p,uint32_t q){//找主运算符
 	// debug;
 	for(i=q;i>=p;i--){
 		debug
+		printf("token%d: %s",tokens[i].type,tokens[i].str);
 		if(tokens[i].type!=NUM&&tokens[i].type!=NOTYPE){
 			if(tokens[i].type==LEFT){
 				l++;continue;
