b7b6ab8365dd6036b1eeccff10152f8827fb07aa
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 16:58:59 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:58:59 up  3:41,  1 user,  load average: 0.00, 0.00, 0.00 5d2d32ce9045a6d3154a02f77028b88be57ffc0b

--- NEMU2021/nemu/src/monitor/debug/expr.c
+++ NEMU2021/nemu/src/monitor/debug/expr.c
@@ -241,9 +241,9 @@ int eval(p, q) {
 		}
 		else {
 			/* We should do more things here. */
-			uint32_t op=find_domanit(p,q);
+			int op=find_domanit(p,q);
 			// printf("op:%d\n",op);
-			uint32_t val1=eval(p,op-1),val2=eval(op+1,q);
+			int val1=eval(p,op-1),val2=eval(op+1,q);
 			switch(tokens[op].type){
 				// debug;
 				case ADD: return val1+val2;
