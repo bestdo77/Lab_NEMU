91a8ae24637ed49db94982f06c65350355efe7cb
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-30 22:24:26 +0800

    >  compile NEMU 3023244355 sw7777 Linux sw7777-virtual-machine 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:24:26 up 12:02,  1 user,  load average: 2.49, 1.61, 0.82 3ba7ada8d7c835ba7b9933686e2aba5761d7df2f

--- NEMU2021/nemu/src/monitor/debug/watchpoint.c
+++ NEMU2021/nemu/src/monitor/debug/watchpoint.c
@@ -73,12 +73,14 @@ int judge_watchpoints(){
     while (current != NULL) {
 		bool success;
         int value=expr(current->args,&success);
+		printf("1\n");
 		printf("point:%d,value:%d,current:%d\n",current->NO,value,current->value);
 		if(value!=current->value){
 			return current->NO;
 		}else{
 			current->value=value;
 		}
+		current=head->next;
     }
 	return 0;
 }
